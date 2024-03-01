a0150f0ed624b0c28c7c63c6573ba65c44658a36
iAmSoloDev <107878651+iAmSoloDev@users.noreply.github.com>
2024-03-01 10:42:30 +0000

    remove unwanted files

---
+++ index.js
@@ -0,0 +1,25 @@
+const num1 = 2
+const num2 = 2
+const operator = ''
+
+function add(a, b) {
+    return a + b    
+}
+
+function subtract(a, b) {
+    return a - b    
+}
+
+function multiply(a, b) {
+    return a * b    
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+console.log(divide(2, 5))
+
+function operate(operator, a, b) {
+    add()
+}
